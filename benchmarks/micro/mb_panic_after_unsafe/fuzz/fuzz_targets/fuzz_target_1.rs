6cf035352d6919f2e77481afc91d28282b8d0ccc
Pang <2055761346@qq.com>
2026-05-13 00:38:16 +0800

    Add fuzz target

---
+++ benchmarks/micro/mb_panic_after_unsafe/fuzz/fuzz_targets/fuzz_target_1.rs
@@ -0,0 +1,13 @@
+#![no_main]
+use libfuzzer_sys::fuzz_target;
+use mb_panic_after_unsafe::process;
+use rustdpr_trace::{init_trace, install_panic_hook};
+use std::path::PathBuf;
+
+fuzz_target!(|data: &[u8]| {
+    let _ = init_trace(PathBuf::from("fuzz_trace.jsonl"));
+    install_panic_hook();
+    let _ = std::panic::catch_unwind(|| {
+        let _ = process(data);
+    });
+});
